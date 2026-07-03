cc1f969b7beca3401235e91ba5177978130ca268
Nour Fakih <fakihnourzahraa@gmail.com>
2026-07-03 11:48:11 +0300

    reverting

--- index.php
+++ index.php
@@ -40,8 +40,6 @@
 /* Do we need to run the installer? */
 
 include_once('./config.php');
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
 if (!file_exists('INSTALL_BLOCK') && !isset($_POST['performMaintenence']))
 {
